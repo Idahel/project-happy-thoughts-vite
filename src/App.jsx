b524dc951fed64ccbb35ac701bc7735111c05ff5
Idahel <idakatrinhellgren@gmail.com>
2023-11-03 13:48:05 +0100

    update nr 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 
 import Header from './assets/Components/Header.jsx'
 import ThoughtList from './assets/Components/ThoughtList.jsx';
